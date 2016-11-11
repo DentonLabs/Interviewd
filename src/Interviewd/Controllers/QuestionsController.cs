a1704a418609acca6eebab7305d3b0af7e58b18e
Alex Denton <alex.denton.dev@powerdms.com>
2016-11-11 17:47:12 -0500

    Added simple GET questions route

---
+++ src/Interviewd/Controllers/QuestionsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Interviewd.Controllers
+{
+    [Route("questions")]
+    public class QuestionsController : Controller
+    {
+        [HttpGet]
+        public async Task<IEnumerable<string>> GetQuestions()
+        {
+            return new List<string>()
+            {
+                "test1",
+                "test2"
+            };
+        }
+    }
+}
