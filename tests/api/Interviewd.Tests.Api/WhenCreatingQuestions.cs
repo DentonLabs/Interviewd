8395b19cd95c592e5ae4b14a7c07090582176e13
Alex Denton <alex.denton.dev@gmail.com>
2017-07-15 21:01:14 -0400

    Configured API port

--- tests/api/Interviewd.Tests.Api/WhenCreatingQuestions.cs
+++ tests/api/Interviewd.Tests.Api/WhenCreatingQuestions.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using NUnit.Framework;
 using Shouldly;
 
 namespace Interviewd.Tests.Api
@@ -9,6 +12,9 @@ namespace Interviewd.Tests.Api
         public void ShouldBeAbleToCreateAQuestion()
         {
             true.ShouldBeTrue();
+
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("localhost:9005");
         }
     }
 }
