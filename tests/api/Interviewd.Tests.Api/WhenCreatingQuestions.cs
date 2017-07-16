4cac43c1bf0ef83fc14004c90fb46e9e4105e211
Alex Denton <alex.denton.dev@gmail.com>
2017-07-15 20:43:54 -0400

    Setup for tests

---
+++ tests/api/Interviewd.Tests.Api/WhenCreatingQuestions.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace Interviewd.Tests.Api
+{
+    public class WhenCreatingQuestions
+    {
+        [Test]
+        public void ShouldBeAbleToCreateAQuestion()
+        {
+            true.ShouldBeTrue();
+        }
+    }
+}
