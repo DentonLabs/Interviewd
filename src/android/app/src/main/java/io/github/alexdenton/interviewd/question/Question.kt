14e376d65398ab1b0af5bead3d8802c183cfd819
Ryan Fonzi <ryan.fonzi@gmail.com>
2017-07-24 15:16:25 -0400

    Add time estimate to Question

--- src/android/app/src/main/java/io/github/alexdenton/interviewd/question/Question.kt
+++ src/android/app/src/main/java/io/github/alexdenton/interviewd/question/Question.kt
@@ -2,5 +2,6 @@ package io.github.alexdenton.interviewd.question
 
 data class Question(
         val name: String,
-        val description: String
+        val description: String,
+        val timeEstimate: Int
 )
