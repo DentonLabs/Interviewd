7ac330a9f3299542415ae35de4d2a5605f630e02
Ryan Fonzi <ryan.fonzi@gmail.com>
2017-07-26 19:37:50 -0400

    InterviewCreator: Add import existing interview, swapping questions, and changing name

--- src/android/app/src/main/java/io/github/alexdenton/interviewd/interview/InterviewCreator.kt
+++ src/android/app/src/main/java/io/github/alexdenton/interviewd/interview/InterviewCreator.kt
@@ -5,9 +5,25 @@ import io.github.alexdenton.interviewd.question.Question
 /**
  * Created by ryan on 7/24/17.
  */
-class InterviewCreator(val name: String) {
+class InterviewCreator(var name: String) {
+    constructor(interview: Interview) : this(interview.name) {
+        for (question in interview.questions) addQuestion(question)
+    }
+
     val internalList = mutableListOf<Question>()
 
-    fun  addQuestion(question: Question) = internalList.add(question)
-    fun  create(): Interview = Interview(name, internalList.toList())
+    fun addQuestion(question: Question) = internalList.add(question)
+
+    fun create(): Interview = Interview(name, internalList.toList())
+
+    fun swap(indexOne: Int, indexTwo: Int) = internalList.apply {
+        val temp: Question = this[indexOne]
+        this[indexOne] = this[indexTwo]
+        this[indexTwo] = temp
+    }
+
+    fun changeName(newName: String) {
+        name = newName
+    }
+
 }
