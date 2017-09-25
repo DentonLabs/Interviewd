6f8afe2747766e1ccb6eb476796010c0f1e436c5
Ryan Fonzi <ryan.fonzi@gmail.com>
2017-09-25 16:39:41 -0400

    Refactor Templates fragment on dashboard to MVVM

--- src/android/app/src/main/java/io/github/alexdenton/interviewd/dashboard/templates/TemplatesFragment.kt
+++ src/android/app/src/main/java/io/github/alexdenton/interviewd/dashboard/templates/TemplatesFragment.kt
@@ -1,6 +1,7 @@
 package io.github.alexdenton.interviewd.dashboard.templates
 
 
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
@@ -12,16 +13,24 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.TextView
+import com.github.salomonbrys.kodein.LazyKodein
+import com.github.salomonbrys.kodein.android.appKodein
+import com.jakewharton.rxbinding2.view.clicks
 import io.github.alexdenton.interviewd.R
-import io.github.alexdenton.interviewd.api.TemplateRetrofitRepository
 import io.github.alexdenton.interviewd.createtemplate.CreateTemplateActivity
 import io.github.alexdenton.interviewd.interview.Template
+import io.github.rfonzi.rxaware.BaseFragment
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 
 /**
  * A simple [Fragment] subclass.
  */
-class TemplatesFragment : Fragment() {
+class TemplatesFragment : BaseFragment() {
+
+    lateinit var vm: TemplatesViewModel
 
     lateinit var recyclerView: RecyclerView
     lateinit var progressBar: ProgressBar
@@ -29,37 +38,44 @@ class TemplatesFragment : Fragment() {
     lateinit var addTemplateFab: FloatingActionButton
     var templates: List<Template> = emptyList()
     val numCols = 2
-    lateinit var presenter: TemplatesPresenter
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater!!.inflate(R.layout.fragment_templates, container, false)
 
-        presenter = TemplatesPresenter(TemplateRetrofitRepository(context), this)
+        vm = ViewModelProviders.of(this).get(TemplatesViewModel::class.java)
+        vm.initWith(LazyKodein(appKodein))
 
         recyclerView = view.findViewById(R.id.templates_recyclerView)
         progressBar = view.findViewById(R.id.templates_progressBar)
         errorTextView = view.findViewById(R.id.templates_failedToGetTemplatesText)
         addTemplateFab = view.findViewById(R.id.templates_addTemplateFab)
 
-        addTemplateFab.setOnClickListener { startActivity(Intent(context, CreateTemplateActivity::class.java)) }
-
         recyclerView.layoutManager = GridLayoutManager(context, numCols)
         recyclerView.adapter = TemplatesAdapter(templates)
 
-        presenter.getAllTemplates()
+        vm.exposeAddFab(addTemplateFab.clicks())
+        vm.getTemplatesObservable()
+                .timeout(5, TimeUnit.SECONDS)
+                .firstElement()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe ({foundTemplates(it)},
+                        {couldNotConnect()})
+                .lifecycleAware()
+
 
         return view
     }
 
-    fun onCouldNotConnect() {
+    fun couldNotConnect() {
         recyclerView.visibility = View.GONE
         progressBar.visibility = View.GONE
         errorTextView.visibility = View.VISIBLE
     }
 
 
-    fun onFoundTemplates(list: List<Template>) {
+    fun foundTemplates(list: List<Template>) {
         recyclerView.adapter = TemplatesAdapter(list)
         recyclerView.adapter.notifyDataSetChanged()
         progressBar.visibility = View.GONE
