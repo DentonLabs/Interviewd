8694a1609eb1c519b208bdd37874d1911383a223
Ryan Fonzi <ryan.fonzi@gmail.com>
2017-09-24 21:05:06 -0400

    Change Create Question flow to use MVVM

--- src/android/app/src/main/java/io/github/alexdenton/interviewd/createquestion/CreateQuestionActivity.kt
+++ src/android/app/src/main/java/io/github/alexdenton/interviewd/createquestion/CreateQuestionActivity.kt
@@ -1,17 +1,25 @@
 package io.github.alexdenton.interviewd.createquestion
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.NavUtils
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import com.github.salomonbrys.kodein.LazyKodein
+import com.github.salomonbrys.kodein.android.appKodein
+import com.jakewharton.rxbinding2.view.clicks
+import com.jakewharton.rxbinding2.widget.RxTextView
+import com.jakewharton.rxbinding2.widget.textChanges
 import io.github.alexdenton.interviewd.R
 import io.github.alexdenton.interviewd.api.QuestionRetrofitRepository
+import io.github.rfonzi.rxaware.BaseActivity
 
-class CreateQuestionActivity : AppCompatActivity() {
+class CreateQuestionActivity : BaseActivity() {
 
-    lateinit var presenter: CreateQuestionPresenter
+    lateinit var vm: CreateQuestionViewModel
+
     lateinit var nameField: EditText
     lateinit var durField: EditText
     lateinit var descField: EditText
@@ -20,25 +28,29 @@ class CreateQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_question)
+        vm = ViewModelProviders.of(this).get(CreateQuestionViewModel::class.java)
+        vm.withKodein(LazyKodein(appKodein))
 
         actionBar?.setDisplayHomeAsUpEnabled(true)
 
-        presenter = CreateQuestionPresenter(this, QuestionRetrofitRepository(this))
         nameField = findViewById(R.id.createQuestionNameField)
         durField = findViewById(R.id.createQuestionDurationField)
         descField = findViewById(R.id.createQuestionDescField)
         submitButton = findViewById(R.id.createQuestionSubmitButton) // TODO: Use a different naming convention for ids
 
-        submitButton.setOnClickListener { presenter.submitQuestion() }
-    }
+        vm.exposeNameField(nameField.textChanges().map { it.toString() })
+        vm.exposeEstimateField(durField.textChanges().filter { it.isNotBlank() }.map { it.toString().toInt() })
+        vm.exposeDescField(descField.textChanges().map { it.toString() })
+        vm.exposeSubmitButton(submitButton.clicks())
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item?.itemId) {
-            R.id.home -> {
-                NavUtils.navigateUpFromSameTask(this)
-                return true
-            }
-            else -> return super.onOptionsItemSelected(item)
-        }
+        vm.clearSignal
+                .subscribe {
+                    nameField.setText("")
+                    durField.setText("")
+                    descField.setText("")
+                }
+                .lifecycleAware()
+
+
     }
 }
