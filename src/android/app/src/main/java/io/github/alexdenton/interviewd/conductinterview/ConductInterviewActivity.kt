090bbe7582f3cb36fa3ccf5e47fd4f2fc42f025b
Ryan Fonzi <ryan.fonzi@gmail.com>
2017-09-27 20:39:58 -0400

    ConductInterview: Disable swiping/Add next question button

--- src/android/app/src/main/java/io/github/alexdenton/interviewd/conductinterview/ConductInterviewActivity.kt
+++ src/android/app/src/main/java/io/github/alexdenton/interviewd/conductinterview/ConductInterviewActivity.kt
@@ -2,13 +2,18 @@ package io.github.alexdenton.interviewd.conductinterview
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.support.v4.view.ViewPager
+import android.widget.ImageButton
 import android.widget.TextView
 import com.github.salomonbrys.kodein.LazyKodein
 import com.github.salomonbrys.kodein.android.appKodein
+import com.jakewharton.rxbinding2.support.v4.view.pageSelections
+import com.jakewharton.rxbinding2.view.clicks
 import io.github.alexdenton.interviewd.R
 import io.github.alexdenton.interviewd.interview.Interview
 import io.github.rfonzi.rxaware.BaseActivity
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class ConductInterviewActivity : BaseActivity() {
 
@@ -16,7 +21,9 @@ class ConductInterviewActivity : BaseActivity() {
 
     lateinit var candidateNameText: TextView
     lateinit var interviewTitleText: TextView
-    lateinit var questionViewPager: ViewPager
+    lateinit var questionViewPager: QuestionViewPager
+    lateinit var nextQuestionName: TextView
+    lateinit var nextQuestionButton: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,15 +37,33 @@ class ConductInterviewActivity : BaseActivity() {
         candidateNameText = findViewById(R.id.conductInterview_candidateName)
         interviewTitleText = findViewById(R.id.conductInterview_interviewPosition)
         questionViewPager = findViewById(R.id.conductInterview_questionViewPager)
+        nextQuestionName = findViewById(R.id.conductInterview_nextQuestionName)
+        nextQuestionButton = findViewById(R.id.conductInterview_nextButton)
 
         val viewPagerAdapter = QuestionPagerAdapter(supportFragmentManager, vm.interview.questions)
         questionViewPager.adapter = viewPagerAdapter
 
-        setupStaticInfo(vm.interview)
+        vm.getNextQuestionStringObservable()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { updateNextQuestion(it) }
+                .lifecycleAware()
+
+        vm.getNextPageSignal()
+                .subscribe { questionViewPager.currentItem = it }
+
+        vm.exposeNextClicks(nextQuestionButton.clicks())
+        vm.exposeCurrentPage(Observable.concat(Observable.just(vm.currentPage), questionViewPager.pageSelections().skipInitialValue()))
+
+        setupInfo(vm.interview)
 
     }
 
-    fun setupStaticInfo(interview: Interview){
+    private fun updateNextQuestion(nextQuestionString: String) {
+        nextQuestionName.text = nextQuestionString
+    }
+
+    fun setupInfo(interview: Interview){
         candidateNameText.text = interview.candidate.toString()
         interviewTitleText.text = interview.name
     }
--- src/android/app/src/main/java/io/github/alexdenton/interviewd/conductinterview/ConductInterviewViewModel.kt
+++ src/android/app/src/main/java/io/github/alexdenton/interviewd/conductinterview/ConductInterviewViewModel.kt
@@ -2,18 +2,28 @@ package io.github.alexdenton.interviewd.conductinterview
 
 import com.github.salomonbrys.kodein.LazyKodein
 import com.github.salomonbrys.kodein.instance
+import com.jakewharton.rxbinding2.InitialValueObservable
+import com.jakewharton.rxrelay2.PublishRelay
 import io.github.alexdenton.interviewd.api.InterviewRepository
 import io.github.alexdenton.interviewd.interview.Interview
+import io.github.alexdenton.interviewd.question.Question
 import io.github.rfonzi.rxaware.BaseViewModel
+import io.reactivex.Observable
 
 /**
  * Created by ryan on 9/27/17.
  */
 class ConductInterviewViewModel : BaseViewModel() {
 
-    lateinit var interviewRepo: InterviewRepository
-
+    private lateinit var interviewRepo: InterviewRepository
     lateinit var interview: Interview
+
+    var currentPage = 0
+    private val nextQuestionString: PublishRelay<String> = PublishRelay.create()
+    private val nextPageSignal: PublishRelay<Int> = PublishRelay.create()
+
+    fun getNextQuestionStringObservable(): Observable<String> = nextQuestionString
+    fun getNextPageSignal(): Observable<Int> = nextPageSignal
 
     fun initWith(kodein: LazyKodein){
         interviewRepo = kodein.invoke().instance()
@@ -22,4 +32,28 @@ class ConductInterviewViewModel : BaseViewModel() {
     fun useInterview(interview: Interview) {
         this.interview = interview
     }
+
+    fun exposeCurrentPage(pageSelections: Observable<Int>) = pageSelections
+            .subscribe {
+                when(it){
+                    interview.questions.size - 1 -> nextQuestionString.accept("Finish interview")
+                    else -> nextQuestionString.accept(interview.questions[it + 1].name)
+                }
+                currentPage = it
+            }
+            .lifecycleAware()
+
+    fun exposeNextClicks(clicks: Observable<Unit>) = clicks
+            .subscribe {
+                if(currentPage == interview.questions.size - 1) {
+                    toast("Finished the interview")
+                    navigateUp()
+                }
+                else {
+                    nextPageSignal.accept(currentPage + 1)
+                }
+            }
+            .lifecycleAware()
+
+
 }
