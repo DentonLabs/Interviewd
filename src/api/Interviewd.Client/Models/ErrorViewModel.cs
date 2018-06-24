ca2f3a524bee92825fc6df9ba3c8a8fbc9fe9c5c
Alex Denton <alex.denton.dev@gmail.com>
2018-06-24 11:35:37 -0400

    Added default MVC client

---
+++ src/api/Interviewd.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Interviewd.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
