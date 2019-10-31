4332c4171ea2a725b2b525b2dba6cff658eede3f
Ryan Fonzi <ryan.fonzi@gmail.com>
2019-10-31 16:52:46 -0400

    Remove android app from project

--- src/android/app/src/main/java/io/github/alexdenton/interviewd/interview/conduct/InterviewSignal.kt
+++
@@ -1,9 +0,0 @@
-package io.github.alexdenton.interviewd.interview.conduct
-
-/**
- * Created by ryan on 9/28/17.
- */
-enum class InterviewSignal {
-    START,
-    STOP
-}
--- src/android/app/src/main/java/io/github/alexdenton/interviewd/question/detail/QuestionDetailRouter.kt
+++
@@ -1,9 +0,0 @@
-package io.github.alexdenton.interviewd.question.detail
-
-/**
- * Created by ryan on 10/2/17.
- */
-enum class QuestionDetailRouter {
-    SHOW,
-    EDIT
-}
